06f8f78af067f119d1bcaa44d62aa1e72d3bf8d4
Facelless <facelless0@gmail.com>
2025-11-13 16:35:08 +0000

    initial commit

---
+++ domain/user/entity.go
@@ -0,0 +1,12 @@
+package user
+
+type User struct {
+	Username string
+	Email    string
+	Password string
+	Id       string
+	Icon     string
+	Bio      string
+	Url_port string
+	Status   string
+}
